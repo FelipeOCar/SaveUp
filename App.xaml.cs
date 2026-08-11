287ac88f09b9cc2bab3d7be5f5611d3b3ccd2a7f
FelipeOCar <felipe.oliveiradecarvalho@student.ipso.ch>
2026-08-11 11:59:43 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace SaveUp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
